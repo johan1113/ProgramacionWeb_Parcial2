14d3e9c1954707a2405e7e5dc589d311c1ad78bd
Johan Sebastian Medina Castano <1107521935@academico.icesi.edu.co>
2018-11-16 08:48:07 -0500

    se ha agregado la funcion de conteo de visitas de cada pagina y la escritura del archivo txt que almacena las visitas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const app = express();
+const fs = require('fs');
 
 //para definir la carpeta publica
 app.use(express.static('public'));
@@ -9,17 +10,42 @@ app.engine('handlebars', hbs());
 //para setear el motor de render a utilizar
 app.set('view engine', 'handlebars');
 
-app.get('/home',function(req , res){
+var homeTime = 0;
+var landingTime = 0;
+var shopTime = 0;
+
+function calculatePagesTimes() {
+    var message = "Home Page: "+homeTime+" visits\n"+"Landing Page: "+landingTime+" visits\n"+"Shop Page: "+shopTime+" visits";
+    console.log(message);
+    /*
+    console.log("Home Page: "+homeTime+" visits");
+    console.log("Landing Page: "+landingTime+" visits");
+    console.log("Shop Page: "+shopTime+" visits");
+    */
+
+    const data = new Uint8Array(Buffer.from(message));
+    fs.writeFile('message.txt', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
+app.get('/',function(req , res){
     res.render('home');
+    homeTime++;
+    calculatePagesTimes();
 });
 
 app.get('/landing',function(req , res){
     res.render('landing');
+    landingTime++;
+    calculatePagesTimes();
 });
 
 app.get('/shop',function(req , res){
     res.render('shop');
+    shopTime++;
+    calculatePagesTimes();
 });
-
 
 app.listen(5000);
